832150d1690c883f81b8ca9eb661de65ec46eebe
Heitor <134107213+HeitorTeixeiradeMenezes@users.noreply.github.com>
2024-06-04 16:40:31 -0300

    lessons

---
+++ 1-Intrroducao/2-variaveis.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int a = 5;
+float num = 2.5;
+double d = 1.1;
+char c = 't';
+
+int main()
+{
+    printf("o valor da variavel 'a' e: %d\n",a);
+    printf("o valor da variavel 'num' e: %.1f\n",num);
+    printf("o valor da variavel 'd' e: %.2f\n",d);
+     printf("o valor da variavel 'c' e: %c\n",c);
+    return 0;
+}

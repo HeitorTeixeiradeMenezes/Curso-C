e427b63baf98a82413abfbad2e4a3418f7250493
Heitor <134107213+HeitorTeixeiradeMenezes@users.noreply.github.com>
2024-06-05 14:34:08 -0300

    lessons

---
+++ 1-Intrroducao/4-Condicoes.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int a, b, c;
+    printf("Digite um numero:\n");
+    scanf("%d", &a);
+    printf("Digite um numero:\n");
+    scanf("%d", &b);
+    printf("Digite um numero:\n");
+    scanf("%d", &c);
+    if (a==b && b==c)
+    {
+        printf("Todos os valores tem o valor igual\n");
+    }
+    
+
+
+
+    return 0;
+}
